dce035489a89093aba1cb43d5002d1aded73ef07
jasmin3ashl3y <jasmine@ashleyit.com>
2022-01-24 15:31:37 -0500

    concurrently integrate client and server sides

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container">&copy;2022 by jasmin3ashl3y</div>
+    </footer>
+  );
+};
+
+export default Footer;
