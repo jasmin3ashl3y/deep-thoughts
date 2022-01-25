421dcb45ada1449f6a86028d4ba9ac14d8554378
jasmin3ashl3y <jasmine@ashleyit.com>
2022-01-24 20:56:07 -0500

    authenticate profile after login etc

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
+import Auth from '../utils/auth';
+
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -17,12 +22,18 @@ const Login = (props) => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    // clear form values
-    setFormState({
-      email: '',
-      password: '',
-    });
+
+    try {
+      const { data } = await login({
+        variables: { ...formState }
+      });
+  
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
   };
+
 
   return (
     <main className='flex-row justify-center mb-4'>
@@ -53,6 +64,7 @@ const Login = (props) => {
                 Submit
               </button>
             </form>
+            {error && <div>Login failed</div>}
           </div>
         </div>
       </div>
